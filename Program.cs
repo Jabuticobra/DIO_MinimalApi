509ed79921e810693daa827e11d794f303325be8
Jabuticobra <matheuslo.bio@outlook.com>
2025-10-06 18:47:05 -0300

    Ajustando certas coisas

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
+using minimalapi_api.infraestrutura.Db;
+using minimalapi_api.Dominio.DTOs;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbContexto>(options =>
+{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"), ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    );
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapPost("/login", (LoginDTO) =>
-{
+app.MapPost("/login", (minimalapi_api.Dominio.DTOs.LoginDTO LoginDTO) => {
     if (LoginDTO.Email == "adm@teste.com" && LoginDTO.Senha == "123456")
         return (Task)Results.Ok("login com sucesso");
     else
